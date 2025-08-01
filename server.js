6168f31455e6eeca53a773e9855704f7fa373fd4
dhanashriikadam <patilkaruna222@gmail.com>
2025-08-01 14:38:24 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from "express"; 
+import dotenv from "dotenv";
+import connectdb from "./db/dbconnect.js";
+import userrouter from './router/userroutes.js';
+import jobrouter from './router/jobsroutes.js';
+import jobapplictionrouter from './router/jobapplicationroutes.js'
+import cors from 'cors'
+const app = express();
+dotenv.config();//load env variable
+connectdb();//connect to database
+//MIDDLEWARES
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.status(200).send("Server is running fine");
+})
+app.get('/api/home',(req,res)=>{
+    res.status(200).send("Home page route checking");
+})
+//ROUTES
+app.use('/api',userrouter)
+app.use('/api',jobrouter)
+app.use('/api',jobapplictionrouter)
+let port=process.env.port ||5051;
+app.listen(port, ()=>{
+    console.log(`Server running on http://localhost:${port}`);
+});
